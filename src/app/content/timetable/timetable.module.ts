45ed82a29407e96b51e28192e43542025a9fd55b
iamprincyscalelot <dprincy.scalelot@gmail.com>
2024-08-27 17:28:43 +0530

    add timetable list design

--- src/app/content/timetable/timetable.module.ts
+++ src/app/content/timetable/timetable.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimetableComponent } from './timetable.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { StudentTimetableListComponent } from './student-timetable-list/student-timetable-list.component';
+import { TeacherTimetableListComponent } from './teacher-timetable-list/teacher-timetable-list.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 const routes:Routes = [
   {
@@ -12,11 +20,19 @@ const routes:Routes = [
 
 @NgModule({
   declarations: [
-    TimetableComponent
+    TimetableComponent,
+    StudentTimetableListComponent,
+    TeacherTimetableListComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    MatTableModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class TimetableModule { }
