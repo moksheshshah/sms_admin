78d9f89cc16316c312f2d8c49cef53577c4640eb
iamprincyscalelot <dprincy.scalelot@gmail.com>
2024-08-27 11:25:17 +0530

    add change on student form

--- src/app/content/student/add-edit-student/add-edit-student.component.ts
+++ src/app/content/student/add-edit-student/add-edit-student.component.ts
@@ -30,6 +30,47 @@ export class AddEditStudentComponent {
   varientsList: any = [];
   @ViewChild('allSelected') allSelected: MatOption | any;
   maultivariants: any = [];
+  sectionList:any = [
+    { key:'A', value:'A' }, 
+    { key:'B', value:'B' }, 
+    { key:'C', value:'C' }, 
+    { key:'D', value:'D' }, 
+    { key:'E', value:'E' }, 
+  ];
+  classList:any = [
+    { key:'1st', value:'1' }, 
+    { key:'2nd', value:'2' }, 
+    { key:'3rd', value:'3' }, 
+    { key:'4th', value:'4' }, 
+    { key:'5th', value:'5' }, 
+  ];
+  yearList:any = [
+    { key:'2023 - 2024', value:'2023 - 2024' },
+    { key:'2022 - 2023', value:'2022 - 2023' },
+    { key:'2021 - 2022', value:'2021 - 2022' },
+    { key:'2020 - 2021', value:'2020 - 2021' },
+  ]; 
+  genderList:any = [
+    { key:'Male', value:'male' },
+    { key:'Female', value:'female' },
+    { key:'Other', value:'other' },
+  ];
+  bloodGroupList:any = [
+    { key:'A+', value:'1' }, 
+    { key:'B+', value:'2' }, 
+    { key:'O+', value:'3' },  
+    { key:'A-', value:'4' }, 
+    { key:'B-', value:'5' }, 
+    { key:'O-', value:'6' },  
+  ];
+  religionList:any = [
+    { key:'Hindu', value:'hindu' },
+    { key:'Muslim', value:'muslim' },
+    { key:'Christian', value:'christian' },
+    { key:'Jain', value:'jain' },
+    { key:'Sikh', value:'sikh' },
+    { key:'Buddhist ', value:'buddhist ' },
+  ];
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -96,11 +137,44 @@ export class AddEditStudentComponent {
 
   prepareAddEditExpenseForm() {
     this.productCouponForm = this._formBuilder.group({
-      banner: [''],
-      school_name: ['', [Validators.required]],
-      phone: ['', [Validators.required]],
+      admission_no: ['',Validators.required],
+      roll_no: ['',Validators.required],
+      starttime: ['', Validators.required],
+      classes: ['', Validators.required],
+      sections: ['', Validators.required],
+      enrolled_year: ['', Validators.required],
+      student_name: ['', [Validators.required]],
       email: [null, [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
+      gender: ['',Validators.required],
+      dob: ['',Validators.required],
+      blood_group: ['',Validators.required],
+      hight: ['',Validators.required],
+      weight: ['',Validators.required],
+      father_name: ['',Validators.required],
+      f_occupation: ['', [Validators.required]],
+      f_mobile_no: ['', [Validators.required]],
+      f_qualification: ['', [Validators.required]],
+      f_email: ['', [Validators.required]],
+      f_address: ['', [Validators.required]],
+      mother_name: ['', [Validators.required]],
+      m_occupation: ['', [Validators.required]],
+      m_mobile_no: ['', [Validators.required]],
+      m_qualification: ['', [Validators.required]],
+      m_email: ['', [Validators.required]],
+      m_address: ['', [Validators.required]],
+      nationality: ['', [Validators.required]],
+      religion: ['', [Validators.required]],
+      cast: ['', [Validators.required]],
+      bank_name: ['', [Validators.required]],
+      bank_branch: ['', [Validators.required]],
+      account_no: ['', [Validators.required]],
+      ifsc_code: ['', [Validators.required]],
       address: ['', [Validators.required]],
+      pin_code: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+      mobile_no: ['', [Validators.required]],
     });
   }
 
