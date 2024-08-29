7215092f5e96d73373478f0d5373149a58143ea2
dprashant <dprashant.scalelot@gmail.com>
2024-08-29 10:20:06 +0530

    changes on desgin

--- src/app/content/attendance/attendance.component.ts
+++ src/app/content/attendance/attendance.component.ts
@@ -1,4 +1,7 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { CONSTANTS } from '../../common/constants';
 
 @Component({
   selector: 'app-attendance',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './attendance.component.scss'
 })
 export class AttendanceComponent {
-
+  classList:any = [
+    { key:'1st', value:'1' }, 
+    { key:'2nd', value:'2' }, 
+    { key:'3rd', value:'3' }, 
+    { key:'4th', value:'4' }, 
+    { key:'5th', value:'5' }, 
+  ];
+  sectionList:any = [
+    { key:'A', value:'A' }, 
+    { key:'B', value:'B' }, 
+    { key:'C', value:'C' }, 
+    { key:'D', value:'D' }, 
+    { key:'E', value:'E' }, 
+  ];
+selClass: any;
 }
--- src/app/content/attendance/attendance.module.ts
+++ src/app/content/attendance/attendance.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AttendanceComponent } from './attendance.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 const routes:Routes = [
   {
@@ -17,6 +23,12 @@ const routes:Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    DropdownModule,
+    MatInputModule,
+    FormsModule,
+    InputTextModule,
+    MatSelectModule,
+    MatDatepickerModule,
   ]
 })
 export class AttendanceModule { }
