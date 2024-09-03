354d56e80b5c53ebd61fdaee5e438f78250af5e2
dprashant <dprashant.scalelot@gmail.com>
2024-09-03 14:24:26 +0530

    free invoice done

--- src/app/content/payment-history/payment-history.component.ts
+++ src/app/content/payment-history/payment-history.component.ts
@@ -1,10 +1,59 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
 
+
+export interface SizeElement {
+  studentName: string;
+  class: string;
+  section: string;
+  admNo: string;
+  invoiceNo: string;
+  totalAmount: string;
+  paid: string;
+  due: string;
+  issuedDate: string;
+  paymentDate: string;
+  mobileNo: string;
+  status: string;
+  action: string;
+  // status:any;
+  // action: any;
+}
+const STDATTENDANCE_DATA:SizeElement[]=[
+  { studentName: 'John Doe',class: '1',section: 'A',admNo: '01',invoiceNo:'01',totalAmount:'5000',paid:'3000',due:'2000',issuedDate:'01 Aug, 2024',paymentDate:'10 Aug, 2024',mobileNo:'9876543232',status:'Paid',action:'' },
+];
 @Component({
   selector: 'app-payment-history',
   templateUrl: './payment-history.component.html',
   styleUrl: './payment-history.component.scss'
 })
 export class PaymentHistoryComponent {
+  SIZE_DATA: SizeElement[] = [];
+  totalSizeData: any;
+  searchSize: any = "";
+  displayedColumns: string[] = ['studentName', 'class', 'section', 'admNo', 'invoiceNo', 'totalAmount','paid','due','issuedDate','paymentDate','mobileNo','status','action'];
+  // sizeData = new MatTableDataSource<SizeElement>(this.SIZE_DATA);
+  sizeData = STDATTENDANCE_DATA;
+  selection = new SelectionModel<SizeElement>(true, []);
+  isTableLoading: boolean = false;
+  classList:any = [
+    { key:'1st', value:'1' }, 
+    { key:'2nd', value:'2' }, 
+    { key:'3rd', value:'3' }, 
+    { key:'4th', value:'4' }, 
+    { key:'5th', value:'5' }, 
+  ];
+  sectionList:any = [
+    { key:'A', value:'A' }, 
+    { key:'B', value:'B' }, 
+    { key:'C', value:'C' }, 
+    { key:'D', value:'D' }, 
+    { key:'E', value:'E' }, 
+  ];
+  sectionstatus:any = [
+    { key:'true', value:'true' }, 
+    { key:'false', value:'alse' }, 
+  ];
+selClass: any;
 
 }
--- src/app/content/payment-history/payment-history.module.ts
+++ src/app/content/payment-history/payment-history.module.ts
@@ -2,6 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaymentHistoryComponent } from './payment-history.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { PaginatorModule } from 'primeng/paginator';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
 
 const routes:Routes = [
   {
@@ -17,6 +28,17 @@ const routes:Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    DropdownModule,
+    InputTextModule,
+    FormsModule,
+    PaginatorModule,
+    InputSwitchModule,
+    InputTextareaModule,
+    MatTableModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatInputModule
   ]
 })
 export class PaymentHistoryModule { }
