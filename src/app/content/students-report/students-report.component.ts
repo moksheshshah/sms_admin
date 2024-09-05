e2db654d3765b95b43d59119f30787585105a68b
zeenalpatel123 <https://github.com>
2024-09-05 17:22:02 +0530

    admin-user component done

--- src/app/content/students-report/students-report.component.ts
+++ src/app/content/students-report/students-report.component.ts
@@ -1,10 +1,210 @@
-import { Component } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { CONSTANTS } from '../../common/constants';
+import { MatSort } from '@angular/material/sort';
+import { Paginator } from 'primeng/paginator';
+import { GlobalFunctions } from '../../common/global-function';
+import { ToastrService } from 'ngx-toastr';
+import { ClassService } from '../class/class.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { CommonModalComponent } from '../../common-modal/common-modal.component';
 
+export interface studentsReportComponent{
+  checkbox:any;
+  photo:any;
+  adms_no:any;
+  roll_no:any;
+  student_name:any;
+  class:any;
+  father_name:any;
+  phone:string;
+  gender:any;
+  address:any;
+  status:any;
+  action:any;
+}
+const STDATTENDANCE_DATA:studentsReportComponent[]=[
+  { checkbox:'',photo: '',adms_no: '01',roll_no: '01',student_name: 'Prayag Savaliya',class:'1st - A',father_name:'Umeshbhai s.',phone:'+91 9876543219',gender:'Male',address:'Pal RTO, Surat -345678',status:'Paid',action:'' },
+  { checkbox:'',photo: '',adms_no: '01',roll_no: '01',student_name: 'Prayag Savaliya',class:'1st - A',father_name:'Umeshbhai s.',phone:'+91 9876543219',gender:'Male',address:'Pal RTO, Surat -345678',status:'Paid',action:'' },
+  { checkbox:'',photo: '',adms_no: '01',roll_no: '01',student_name: 'Prayag Savaliya',class:'1st - A',father_name:'Umeshbhai s.',phone:'+91 9876543219',gender:'Male',address:'Pal RTO, Surat -345678',status:'Paid',action:'' },
+  { checkbox:'',photo: '',adms_no: '01',roll_no: '01',student_name: 'Prayag Savaliya',class:'1st - A',father_name:'Umeshbhai s.',phone:'+91 9876543219',gender:'Male',address:'Pal RTO, Surat -345678',status:'Paid',action:'' },
+  { checkbox:'',photo: '',adms_no: '01',roll_no: '01',student_name: 'Prayag Savaliya',class:'1st - A',father_name:'Umeshbhai s.',phone:'+91 9876543219',gender:'Male',address:'Pal RTO, Surat -345678',status:'Paid',action:'' },
+];
 @Component({
   selector: 'app-students-report',
   templateUrl: './students-report.component.html',
   styleUrl: './students-report.component.scss'
 })
 export class StudentsReportComponent {
+  // STUDENTS_REPORT_DATA:studentsReportComponent[]=[];
+  totalCoupon:any;
+  searchCoupon:any;
+  displayedColumns:string[]=['checkbox','photo','adms_no','roll_no','student_name','class','father_name','phone','gender','address','status','action'];
+  // students_reportData = new MatTableDataSource<studentsReportComponent>(this.STUDENTS_REPORTDATA);
+  selection = new SelectionModel<studentsReportComponent>(true , []);
+  sizeData = STDATTENDANCE_DATA;
+  // name:string = '';
+  constants:any = CONSTANTS;
+  selCouponStatus:any="";
+  pageNo:any;
+  limit:any;
+  isTableLoading:boolean = false; 
+  classList:any = [
+    { key:'1st', value:'1' }, 
+    { key:'2nd', value:'2' }, 
+    { key:'3rd', value:'3' }, 
+    { key:'4th', value:'4' }, 
+    { key:'5th', value:'5' }, 
+  ];
+  sectionList:any = [
+    { key:'A', value:'A' }, 
+    { key:'B', value:'B' }, 
+    { key:'C', value:'C' }, 
+    { key:'D', value:'D' }, 
+    { key:'E', value:'E' }, 
+  ];
+  @ViewChild(MatSort,{static:false}) couponSort!:MatSort;
+  @ViewChild('paginator', { static: true }) paginator!: Paginator
+  selectedClass: any;
+  selectedSection: any;
 
+  constructor(
+    private _globalFunctions:GlobalFunctions,
+    private _toastr:ToastrService,
+    private _couponService:ClassService,
+    private _dialog:MatDialog,
+    private _router: Router,
+  ){}
+  ngOnInit(){
+    this.getAllCouponList();
+  }
+
+
+  onKeySearch(event:any){
+    this.searchCoupon = event?.target?.value;
+    this.getAllCouponList();
+  }
+
+  filterData(){
+    $('.mat-mdc-select-panel.mdc-menu-surface.mdc-menu-surface--open').click(function(){
+      $('.mat-focused').removeClass('mat-focused')
+    })
+    this.getAllCouponList();  
+  }
+
+  getAllCouponList(event:any = ''){
+    this.isTableLoading = true;
+    this.pageNo = event? (event.page + 1) : 1;
+    this.limit = event.rows || 10;
+    let filter = {
+      page: this.pageNo || '1',
+      limit: this.limit || '10',
+      search:this.searchCoupon || '',
+    }
+
+    this._couponService.getSize(filter).subscribe((result:any)=>{
+      if(result && result.IsSuccess){
+        this.totalCoupon = result?.Data?.totalDocs;
+        // this.STUDENTS_REPORT_DATA = result.Data.docs;
+        // this.students_report_Data = new MatTableDataSource<studentsReportComponent>(this.STUDENTS_REPORT_DATA);
+        // this.students_report_Data.sort = this.couponSort;
+        this.isTableLoading = false;
+      } else {
+        this.isTableLoading = false;
+        this._globalFunctions.successErrorHandling(result,this,true)
+      }
+    },(error:any)=>{
+      this.isTableLoading = false;
+      this._globalFunctions.errorHanding(error,this,true);
+    })
+  }
+
+  editStudentReport(event:any,resData:any){
+    event.stopPropagation();
+    this._router.navigate(['student/', resData?.id]);
+  }
+
+
+  changeStatusAction(event:any,resData:any){
+    this.isTableLoading = true;
+    const dialogRef = this._dialog.open(CommonModalComponent,{
+      width:'600px',
+      data:{
+        title:'Confirmation',
+        message:'Are you sure you want to change the status',
+        buttonNames:[{firstBtn:"Update",secondBtn:'Cancel'}]
+      }
+    });
+    dialogRef.afterClosed().subscribe((res)=>{
+      if(res){
+      //   let param = {
+      //     couponid : resData?.id,
+      //   }
+      //   this._couponService.changeCouponStatus(param).subscribe((result:any)=>{
+      //     if(result && result.IsSuccess){
+      //       this._toastr.clear();
+      //       this._toastr.success(result?.Message || "Status updated successfully." , "Success");
+      //       this.getAllCouponList();
+      //       this.isTableLoading = false;
+      //     } else {
+      //       this.getAllCouponList();
+      //       this.isTableLoading = false;
+      //       this._globalFunctions.successErrorHandling(result,this,true);
+      //     }
+      //   },(error:any)=>{
+      //     this.getAllCouponList();
+      //     this.isTableLoading = false;
+      //     this._globalFunctions.errorHanding(error,this,true)
+      //   })
+      // } else {
+      //   this.getAllCouponList();
+      //   this.isTableLoading = false;
+      }
+    });
+  }
+
+  imageOnError(event: any) {
+    event.target.src = this.constants.defaultImage;
+  }
+
+  deleteStudentReport(element:any){
+    this.isTableLoading = true;
+    const dialogRef = this._dialog.open(CommonModalComponent,{
+      width:'410px',
+      height:'fit-content',
+      data:{
+        title:'Confirmation!',
+        message:'Are you sure you want to delete the School? Please note that all data related to this School will be deleted.',
+        buttonNames:[{firstBtn:"Cancel",secondBtn:'Yes, Delete'}]
+      }
+    });
+    dialogRef.afterClosed().subscribe((res)=>{
+      if(res){
+        let param = {
+          couponid : element?.id,
+        }
+      //   this._couponService.changeCouponStatus(param).subscribe((result:any)=>{
+      //     if(result && result.IsSuccess){
+      //       this._toastr.clear();
+      //       this._toastr.success(result?.Message || "Status updated successfully." , "Success");
+      //       this.getAllCouponList();
+      //       this.isTableLoading = false;
+      //     } else {
+      //       this.getAllCouponList();
+      //       this.isTableLoading = false;
+      //       this._globalFunctions.successErrorHandling(result,this,true);
+      //     }
+      //   },(error:any)=>{
+      //     this.getAllCouponList();
+      //     this.isTableLoading = false;
+      //     this._globalFunctions.errorHanding(error,this,true)
+      //   })
+      // } else {
+      //   this.getAllCouponList();
+      //   this.isTableLoading = false;
+      }
+    });
+  }
 }
--- src/app/content/students-report/students-report.module.ts
+++ src/app/content/students-report/students-report.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentsReportComponent } from './students-report.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { Paginator, PaginatorModule } from 'primeng/paginator';
+import { ImageModule } from 'primeng/image';
+import { InputSwitchModule } from 'primeng/inputswitch';
 
 const routes:Routes = [
   {
@@ -17,6 +26,17 @@ const routes:Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    FormsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    PaginatorModule,
+    ImageModule,
+    InputSwitchModule
+
   ]
 })
 export class StudentsReportModule { }
