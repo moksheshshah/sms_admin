3b09e54d623eef732aeadbf427ab566c741f5bad
iamprincyscalelot <dprincy.scalelot@gmail.com>
2024-09-06 12:09:55 +0530

    add changes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogInComponent } from './log-in/log-in.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -9,7 +10,8 @@ import { LogInComponent } from './log-in/log-in.component';
     // LogInComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ]
 })
 export class AuthModule { }
